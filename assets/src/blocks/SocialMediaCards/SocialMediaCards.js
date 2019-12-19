a0fc933f2f15890af1e874fcf499c106e737f9d3
inwerpsel <pieter.vincent1988@gmail.com>
2019-12-19 19:43:08 +0100

    PLANET-4609 Remove focal points fields and various
    * Originally intended to fix multiple bugs, but ended up rewriting most
    of it as that was simply less work then fixing these things in the form
    the code was in. The main problem was that the code was copied from the
    gallery block, and kept the different attributes to store the images,
    instead of using nested attributes which gutenberg supports. That in
    turn meant that the majority of lines of code where dealing with
    splitting these attributes, searching for the element at the right index
    and then combining them again, making reading and debugging the code a
    huge headache.
    * Fix messages not working.

--- assets/src/blocks/SocialMediaCards/SocialMediaCards.js
+++ assets/src/blocks/SocialMediaCards/SocialMediaCards.js
@@ -1,9 +1,6 @@
 import { Component, Fragment } from '@wordpress/element';
 import { Preview } from '../../components/Preview';
-
-
 import {
-  BlockControls,
   MediaUpload,
   MediaUploadCheck
 } from '@wordpress/editor';
@@ -13,8 +10,7 @@ import {
   TextareaControl,
   ServerSideRender,
   Button,
-  Toolbar,
-  IconButton, Tooltip
+  Tooltip
 } from '@wordpress/components';
 
 export class SocialMediaCards extends Component {
@@ -25,56 +21,44 @@ export class SocialMediaCards extends Component {
   renderEdit() {
     const { __ } = wp.i18n;
 
-    const dimensions = { width: 212, height: 212 };
-
-    const { multiple_image, image_data, onDeleteImage } = this.props;
+    const { cards, onDeleteImage } = this.props;
 
     const getImageOrButton = (openEvent) => {
-      if (0 < this.props.image_data.length) {
+      if (cards.length > 0) {
 
-        return (
-
-          this.props.image_data.map((item, index) => {
-            return (
-              <span className="img-wrap">
-                <Tooltip text={__('Remove Image', 'p4ge')}>
-                  <span className="close" onClick={ev => {
-                    onDeleteImage(item.id);
+        return cards.map( ( card, index ) => (
+          <span className="img-wrap">
+                <Tooltip text={ __( 'Remove Image', 'p4ge' ) }>
+                  <span className="close" onClick={ ev => {
+                    onDeleteImage( card.image_id );
                     ev.stopPropagation()
-                  }}>&times;</span>
+                  } }>&times;</span>
                 </Tooltip>
                 <img
-                  src={item.url}
-                  onClick={openEvent}
+                  src={ card.image_url }
+                  onClick={ openEvent }
                   className="gallery__imgs"
-                  key={index}
+                  key={ index }
                   width='150 px'
-                  style={{ padding: '10px 10px' }}
+                  style={ { padding: '10px 10px' } }
                 />
               </span>
-            );
-          })
-
-        );
+        ) )
       }
-      else {
-        return (
-          <div className="button-container">
-            <Button
-              onClick={openEvent}
-              className="button">
-              + {__('Select Images', 'p4ge')}
-            </Button>
 
-            <div>{__('Select images in the order you want them to appear.', 'p4ge')}</div>
-          </div>
-        );
-      }
+      return (
+        <div className="button-container">
+          <Button
+            onClick={openEvent}
+            className="button">
+            + {__('Select Images', 'p4ge')}
+          </Button>
+
+          <div>{__('Select images in the order you want them to appear.', 'p4ge')}</div>
+        </div>
+      );
     };
 
-
-
-    let multiple_image_array = multiple_image ? multiple_image.split(',') : [];
     return (
       <Fragment>
         <div>
@@ -99,8 +83,8 @@ export class SocialMediaCards extends Component {
             <MediaUpload
               title={__('Select Images', 'p4ge')}
               type="image"
-              onSelect={this.props.onSelectImage}
-              value={multiple_image_array}
+              onSelect={this.props.onSelectImages}
+              value={ cards.map( card => card.image_id ) }
               allowedTypes={["image"]}
               multiple="true"
               render={({ open }) => getImageOrButton(open)}
@@ -108,53 +92,43 @@ export class SocialMediaCards extends Component {
           </MediaUploadCheck>
         </div>
 
-        {image_data &&
-          <div
-            className={
-              "wp-block-master-theme-gallery__FocalPointPicker"
-            }
-          >
-            <ul>
-              {image_data.map((item, index) => {
-                return (
-                  <li
-                    key={index}
-                  >
-                    <div className="row">
-                      <div className="col-md-6">
-                        <img
-                          src={item.url}
-                          width={212}
-                          height={212}
-                        />
-                      </div>
-                      <div className="col-md-6">
-                        <TextareaControl
-                          label={__('Social message', 'p4ge')}
-                          placeholder={__('Enter message', 'p4ge')}
-                          help={__('Optional. This message will be added as a quote on Facebook and Twitter shares.', 'p4ge')}
-                          value={item.message}
-                          onChange={this.props.onMessageChange.bind(this, item.id)}
-                          key={item.id + 't1'}
-                        />
-
-                        <TextControl
-                          label={__('Social URL', 'p4ge')}
-                          placeholder={__('Enter URL to share', 'p4ge')}
-                          help={__('Optional. If not specified then the url of the current page will be used.', 'p4ge')}
-                          value={item.social_url}
-                          onChange={this.props.onURLChange.bind(this, item.id)}
-                          key={item.id + 't2'}
-                        />
-                      </div>
+        <div>
+          <ul>
+            { cards.map( ( card, index ) => {
+              return (
+                <li key={ index.toString() }>
+                  <div className="row">
+                    <div className="col-md-6">
+                      <img
+                        src={ card.image_url }
+                        width={ 212 }
+                        height={ 212 }
+                      />
                     </div>
-                    <hr/>
-                  </li>
-                );
-              })}
-            </ul>
-          </div>
-        }
+                    <div className="col-md-6">
+                      <TextareaControl
+                        label={ __( 'Social message', 'p4ge' ) }
+                        placeholder={ __( 'Enter message', 'p4ge' ) }
+                        help={ __( 'Optional. This message will be added as a quote on Facebook and Twitter shares.', 'p4ge' ) }
+                        value={ card.message }
+                        onChange={ this.props.onMessageChange.bind( this, index ) }
+                      />
+
+                      <TextControl
+                        label={ __( 'Social URL', 'p4ge' ) }
+                        placeholder={ __( 'Enter URL to share', 'p4ge' ) }
+                        help={ __( 'Optional. If not specified then the url of the current page will be used.', 'p4ge' ) }
+                        value={ card.social_url }
+                        onChange={ this.props.onURLChange.bind( this, index ) }
+                      />
+                    </div>
+                  </div>
+                  <hr/>
+                </li>
+              );
+            } ) }
+          </ul>
+        </div>
       </Fragment>
     );
   }
@@ -162,19 +136,13 @@ export class SocialMediaCards extends Component {
   render() {
     return (
       <div>
-        {
-          this.props.isSelected
-            ? this.renderEdit()
-            : null
+        { this.props.isSelected &&
+        this.renderEdit()
         }
-        <Preview showBar={this.props.isSelected}>
+        <Preview showBar={ this.props.isSelected }>
           <ServerSideRender
-            block={'planet4-blocks/social-media-cards'}
-            attributes={{
-              multiple_image: this.props.multiple_image,
-              title: this.props.title,
-              description: this.props.description,
-            }}>
+            block={ 'planet4-blocks/social-media-cards' }
+            attributes={ this.props.attributes }>
           </ServerSideRender>
         </Preview>
       </div>
