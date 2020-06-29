9f188541aac0d47b810db8f14bcb767a144225cb
Maud Leray <maudleray035@gmail.com>
2020-06-29 10:46:34 +0200

    PLANET-4801 Start WYSIWYG implementation for Articles block

--- assets/src/frontendIndex.js
+++ assets/src/frontendIndex.js
@@ -1,7 +1,9 @@
 import { SpreadsheetFrontend } from './blocks/Spreadsheet/SpreadsheetFrontend';
+import { ArticlesFrontend } from './blocks/Articles/ArticlesFrontend';
 
 const COMPONENTS = {
-  'planet4-blocks/spreadsheet': SpreadsheetFrontend
+  'planet4-blocks/spreadsheet': SpreadsheetFrontend,
+  'planet4-blocks/articles': ArticlesFrontend
 };
 
 document.querySelectorAll( `[data-render]` ).forEach(
